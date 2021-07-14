67256c1c53175713ffcf541f3a3ba60047d64cb4
Marvio Rocha <marviorocha@marviorocha.com>
2021-07-13 23:55:35 -0300

    Added header components

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,32 @@
 <script>
-  export let name;
 </script>
 
+<header class="text-gray-100 body-font shadow-md bg-blue-400">
+  <div
+    class="container mx-auto flex flex-wrap p-5 px-32 flex-col md:flex-row items-center"
+  >
+    <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+      <i class="fas fa-shopping-bag fa-2x text-red-600" />
+      <span class="ml-3 text-xl">Budget Calculator</span>
+    </a>
+    <nav
+      class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center"
+    >
+      <!-- <a class="mr-5 cursor-pointer hover:text-gray-200">First Link</a>
+      <a class="mr-5 cursor-pointer hover:text-gray-200">Second Link</a>
+      <a class="mr-5 cursor-pointer hover:text-gray-200">Third Link</a>
+      <a class="mr-5 cursor-pointer hover:text-gray-200">Fourth Link</a> -->
+    </nav>
+    <button
+      class="inline-flex items-center bg-blue-800 border-0 py-1 px-3 focus:outline-none hover:bg-blue-700 rounded text-base mt-4 md:mt-0"
+    >
+      <i class="fas fa-plus-square px-2" /> Add Item
+    </button>
+  </div>
+</header>
+
 <main class="container mx-auto px-32">
-  <h1 class="text-3xl text-gray-800">Hello {name}!</h1>
-  <i class="fas fa-home" />
+  <h1 class="text-3xl text-gray-800">
+    <i class="fas fa-home fa-5x text-red-900" /> Hello {name}!
+  </h1>
 </main>
