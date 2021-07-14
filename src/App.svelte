49e27bae7c11d1f4d9f17d9ab98efa9925b15bb3
Marvio Rocha <marviorocha@marviorocha.com>
2021-07-14 10:27:04 -0300

    Add title component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script>
   import Navbar from "./Navbar.svelte";
+  import Title from "./Title.svelte";
 </script>
 
 <Navbar />
-<main class="container mx-auto px-32">
-  <h1 class="text-3xl text-gray-800">
-    <i class="fas fa-home fa-5x text-red-900" /> Hello {name}!
-  </h1>
+<main class="container mx-auto px-32 mt-5">
+  <Title />
 </main>
---
+++ src/Title.svelte
@@ -0,0 +1,3 @@
+<div class="main">
+  <h1 class="text-2xl text-blue-500">Add Expense</h1>
+</div>
