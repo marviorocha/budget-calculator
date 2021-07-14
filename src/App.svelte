57f6323976c90e9c322f4c509ccf87f857ed54b7
Marvio Rocha <marviorocha@marviorocha.com>
2021-07-14 11:14:22 -0300

    Add Expense and ExpenseList Components

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,16 @@
 <script>
   import Navbar from "./Navbar.svelte";
   import Title from "./Title.svelte";
+  import ExpenseList from "./ExpenseList.svelte"
+  import ExpenseDate from "./expenses";
+  let expenses = [...ExpenseDate];
+  console.log(expenses);
 </script>
 
 <Navbar />
 <main class="container mx-auto px-32 mt-5">
   <Title title="Add Expense" />
-  <Title title="Expense List" />
-  <Title />
+  
+  <ExpenseList expense={expenses} />
+ 
 </main>
---
+++ src/ExpenseList.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Title from "./Title.svelte";
+  export let expense = [];
+</script>
+
+<div class="main">
+  {#each expense as item}
+    <Title title={item.name} />
+  {/each}
+</div>
